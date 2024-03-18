151ffd942fa8535476a437ee92505f0bf614ce9e
Carlitosprogramacao <128050081+Carlitosprogramacao@users.noreply.github.com>
2024-03-18 11:14:31 -0300

    Adicionando arquivos da prática

---
+++ main.js
@@ -0,0 +1,13 @@
+function IntroduzirUSer(){
+    const text = prompt("Ola, qual o seu nome?")
+    if(text.length>=6){
+        alert("Voce é bolsonarista nso")
+
+    }
+    else {
+        alert("Seu nome e pequeno")
+    }
+}
+
+
+IntroduzirUSer()   
